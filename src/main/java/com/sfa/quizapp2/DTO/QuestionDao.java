7ce92b67cf19d1732fa54b29bd7424461f3f4302
Rubalkhehra <khehrarubal16@gmail.com>
2025-03-31 15:53:13 -0400

    Adding new quiz services

--- src/main/java/com/sfa/quizapp2/DTO/QuestionDao.java
+++ src/main/java/com/sfa/quizapp2/DTO/QuestionDao.java
@@ -1,3 +1,16 @@
 package com.sfa.quizapp2.DTO;
 
+import com.sfa.quizapp2.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+    List<Question> findByCategoryIgnoreCase(String category);
+
+    @Query(value = "SELECT * FROM question q where q.category=:category order by random() limit :numQ",nativeQuery = true)
+    List<Question> findRandomQuestionByCategory(String category, int numQ);
 }
--- src/main/java/com/sfa/quizapp2/service/QuestionService.java
+++ src/main/java/com/sfa/quizapp2/service/QuestionService.java
@@ -1,3 +1,47 @@
 package com.sfa.quizapp2.service;
 
+import com.sfa.quizapp2.DTO.QuestionDao;
+import com.sfa.quizapp2.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category){
+        Optional<List<Question>> optionalQuestion= Optional.ofNullable(questionDao.findByCategoryIgnoreCase(category));
+        if(optionalQuestion.isPresent()){
+            return new ResponseEntity<>(optionalQuestion.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionDao.save(question);
+            return new ResponseEntity<>("Question added successfully",HttpStatus.CREATED);
+        }catch(Exception e){
+            return new ResponseEntity<>("Error adding question"+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
